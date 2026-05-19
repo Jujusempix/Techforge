5bace4d6a73a81c7e5ac85ee830806d350185300
Julio Cezar <julio.guilherme@grupointegrado.br>
2026-05-19 16:32:16 -0300

    feat: botao de soma e subtracao

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
-console.log("Hello World!");
+// Contador simples: incrementa e decrementa o valor exibido em index.html
 
-console.log("teste novamente");
+const counterEl = document.getElementById('counter');
+const incBtn = document.getElementById('increment');
+const decBtn = document.getElementById('decrement');
+
+let value = 0;
+
+function render() {
+	if (counterEl) counterEl.textContent = value;
+}
+
+if (incBtn) incBtn.addEventListener('click', () => {
+	value += 1;
+	render();
+});
+
+if (decBtn) decBtn.addEventListener('click', () => {
+	value -= 1;
+	render();
+});
+
+// inicializa display
+render();
 
